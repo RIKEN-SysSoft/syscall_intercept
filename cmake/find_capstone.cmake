b57995f7bae8a5582d81ef67c948dcffc142c6f2
Masamichi Takagi <masamichi.takagi@riken.jp>
2021-02-25 12:25:07 +0900

    integrate capstone for RHEL-7 (x86_64)

--- cmake/find_capstone.cmake
+++ cmake/find_capstone.cmake
@@ -34,17 +34,46 @@ find_package(capstone QUIET)
 if(NOT capstone_FOUND)
 	find_package(PkgConfig QUIET)
 	if(PKG_CONFIG_FOUND)
-		pkg_search_module(capstone capstone QUIET)
+		if (CMAKE_SYSTEM_PROCESSOR STREQUAL "x86_64")
+			pkg_search_module(capstone QUIET capstone=3.0.5)
+		elseif (CMAKE_SYSTEM_PROCESSOR STREQUAL "aarch64")
+			pkg_search_module(capstone QUIET capstone>=4.0.1)
+		endif()
 	endif()
 endif()
 
 if(NOT capstone_FOUND)
-	message(FATAL_ERROR
-"Unable to find capstone. Please install pkg-config and capstone development files, e.g.:
-sudo apt-get install pkg-config libcapstone-dev (on Debian, Ubuntu)
-or
-sudo dnf install capstone-devel (on Fedora)
-or see instructions for other ways of installing capstone: http://www.capstone-engine.org/download.html
-If casptone is installed, but cmake didn't manage to find it, there is a slight chance of fixing things by setting some of the following environment variables:
-PKG_CONFIG_PATH, CMAKE_PREFIX_PATH, CMAKE_MODULE_PATH")
+	message("building capstone locally")
+	set(capstone_SUBMODULE TRUE)
+
+	if(CMAKE_SYSTEM_PROCESSOR STREQUAL "x86_64")
+		set(CAPSTONE_VERSION 3.0.5)
+	elseif (CMAKE_SYSTEM_PROCESSOR STREQUAL "aarch64")
+		set(CAPSTONE_VERSION 4.0.1)
+	endif()
+
+	execute_process(COMMAND bash -c "cd ${CMAKE_CURRENT_SOURCE_DIR}/capstone && git checkout ${CAPSTONE_VERSION}"
+		ERROR_VARIABLE CHECKOUT_STDERR ERROR_STRIP_TRAILING_WHITESPACE)
+	if("${CHECKOUT_STDERR}" MATCHES "^error")
+		message(FATAL_ERROR "error: capstone: git checkout ${CAPSTONE_VERSION} failed")
+	endif()
+
+	if(CAPSTONE_VERSION VERSION_EQUAL 3.0.5)
+		execute_process(COMMAND bash -c "cd ${CMAKE_CURRENT_SOURCE_DIR}/capstone && patch -p1 < ${CMAKE_CURRENT_SOURCE_DIR}/capstone.patch"
+			OUTPUT_VARIABLE PATCH_STDOUT OUTPUT_STRIP_TRAILING_WHITESPACE)
+		if(NOT "${PATCH_STDOUT}" MATCHES "^patching")
+			message(FATAL_ERROR "error: capstone: patch failed")
+		endif()
+	endif()
+
+	add_subdirectory(capstone)
+	get_directory_property(capstone_CFLAGS DIRECTORY capstone DEFINITION CMAKE_C_FLAGS_RELEASE)
+# 	message(FATAL_ERROR
+# "Unable to find capstone. Please install pkg-config and capstone development files, e.g.:
+# sudo apt-get install pkg-config libcapstone-dev (on Debian, Ubuntu)
+# or
+# sudo dnf install capstone-devel (on Fedora)
+# or see instructions for other ways of installing capstone: http://www.capstone-engine.org/download.html
+# If casptone is installed, but cmake didn't manage to find it, there is a slight chance of fixing things by setting some of the following environment variables:
+# PKG_CONFIG_PATH, CMAKE_PREFIX_PATH, CMAKE_MODULE_PATH")
 endif()
